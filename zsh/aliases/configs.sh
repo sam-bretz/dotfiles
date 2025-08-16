fa555b341551c8cb574a5159a783a2f90cfa5ca0
Sam Bretz <bretzsam@gmail.com>
2025-08-16 14:49:47 -0700

    update

--- zsh/aliases/configs.sh
+++ zsh/aliases/configs.sh
@@ -1,3 +1,4 @@
 alias zconf="nvim ~/.zshrc"
 alias zs="source ~/.zshrc"
 alias tconf="nvim ~/.tmux.conf"
+alias claude="~/.claude/local/claude"
--- zsh/aliases/dirs.sh
+++ zsh/aliases/dirs.sh
@@ -6,3 +6,10 @@ alias dotdir='cd ~/.dotfiles/'
 alias nvp='cd ~/.dotfiles/nvim/.config/nvim/ && v' # nvim plugin dir
 alias resume='cd ~/projects/resume && v'
 alias todo='nvim $CLOUD_PATH/todo.md'
+
+# AWS EC2 aliases (dynamic IP resolution)
+alias sourcehub-ec2='~/code/sourceHubv2/scripts/aws-deploy/ec2-connect.sh connect'
+alias sourcehub-ip='~/code/sourceHubv2/scripts/aws-deploy/ec2-connect.sh find'
+alias sourcehub-deploy='~/code/sourceHubv2/scripts/aws-deploy/ec2-connect.sh deploy'
+alias sourcehub-status='~/code/sourceHubv2/scripts/aws-deploy/ec2-connect.sh status'
+alias sourcehub-db='PGPASSWORD=SourceHub2024SecurePassword psql -h sourcehub-db.cfm0kcykeufs.us-east-2.rds.amazonaws.com -U sourcehub_admin -d sourcehub'
